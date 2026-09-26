c752a161f2182d430478a936ffe472de4aa2a465
uw0s <mic.nikolaidis@gmail.com>
2026-09-26 20:13:27 +0300

    chore(release): bump version to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "app.astiko"
         minSdk = 26
         targetSdk = 37
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
     }
 
     bundle {
